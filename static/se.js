76159009cc9130708187ed9952053d10aff48755
Michael Kenneth Rasch <miketriticum@gmail.com>
2018-05-07 14:48:19 +0200

    Update se.js

--- static/se.js
+++ static/se.js
@@ -1,10 +1,12 @@
 (function initializeCKEDITOR5 () 
 {  
-  var zEditorA = document.getElementById("#CKEDITOR5x0");
+  var zEditorA = document.getElementById("CKEDITOR5x0");
   zEditorA.addEventlistener("dblclick", 
     function() {
-       InlineEditor.create(zEditorA).then(console.log("A")).catch( error => { console.error( error )})
+       InlineEditor.create(zEditorA)
+                   .then(console.log("A"))
+                   .catch( error => { console.error( error )})
                }
-                           );
+   );
 })
 ();
