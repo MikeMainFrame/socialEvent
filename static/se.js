b4d55f1a40549562987459bf51203a4938789cd7
Michael Kenneth Rasch <miketriticum@gmail.com>
2018-05-07 17:26:35 +0200

    Update se.js

--- static/se.js
+++ static/se.js
@@ -7,8 +7,9 @@
                 { modelElement: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
                 { modelElement: 'heading1', viewElement: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
                 { modelElement: 'heading2', viewElement: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' }
-            ]
-        };
+                      ]
+                }
+             };
     
   var zEditorA = document.getElementById("CKEDITOR5x0");
   InlineEditor.create(zEditorA, eInit).then(console.log("A")).catch( error => { console.error( error )});
