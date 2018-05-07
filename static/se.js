ca45b60b1aa47fe05de00ff1249221d330b232e0
Michael Kenneth Rasch <miketriticum@gmail.com>
2018-05-07 15:01:10 +0200

    Update se.js

--- static/se.js
+++ static/se.js
@@ -1,7 +1,7 @@
 (function initializeCKEDITOR5 () 
 {  
   var zEditorA = document.getElementById("CKEDITOR5x0");
-  zEditorA.addEventlistener("dblclick", 
+  zEditorA.addEventListener("dblclick", 
     function() {
        InlineEditor.create(zEditorA)
                    .then(console.log("A"))
