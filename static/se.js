3f464ccc2bb2434bfc7bbd7aa35eec883ccf6849
Michael Kenneth Rasch <miketriticum@gmail.com>
2018-05-07 17:46:59 +0200

    Update se.js

--- static/se.js
+++ static/se.js
@@ -1,8 +1,8 @@
 (function initializeCKEDITOR5 () 
 {
-  var zEditorA = document.getElementById("CKEDITOR5x1");
+  var zEditorA = document.getElementById("CKEDITOR5x0");
   InlineEditor.create(zEditorA)
-              .then(console.log("CKEDITOR5x1"))
+              .then(console.log("CKEDITOR5x0"))
               .catch( error => { console.error( error )});
     
   var zEditorB = document.getElementById("CKEDITOR5x1");
