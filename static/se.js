512d7088210e2748d7b393e2c40a25e8c958fd24
Michael Kenneth Rasch <miketriticum@gmail.com>
2018-05-07 16:40:25 +0200

    Update se.js

--- static/se.js
+++ static/se.js
@@ -1,12 +1,23 @@
 (function initializeCKEDITOR5 () 
 {  
   var zEditorA = document.getElementById("CKEDITOR5x0");
-  zEditorA.addEventListener("dblclick", 
-    function() {
-       InlineEditor.create(zEditorA)
+  //zEditorA.addEventListener("dblclick", 
+   // function() {
+       InlineEditor.create(zEditorA),
+       {
+        toolbar: [ 'heading', '|', 'bold', 'italic', 'link', 'bulletedList', 'numberedList', 'blockQuote' ],
+        heading: {
+            options: [
+                { modelElement: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
+                { modelElement: 'heading1', viewElement: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
+                { modelElement: 'heading2', viewElement: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' }
+            ]
+        }
+    } )
+    
                    .then(console.log("A"))
                    .catch( error => { console.error( error )})
-               }
+     //          }
    );
   var zEditorB = document.getElementById("CKEDITOR5x1");
   zEditorB.addEventListener("dblclick", 
