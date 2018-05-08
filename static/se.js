7cda702f7894d0336f010dc201ea0e6439a3f25b
Michael Kenneth Rasch <miketriticum@gmail.com>
2018-05-08 09:12:01 +0200

    Update se.js

--- static/se.js
+++ static/se.js
@@ -1,16 +1,22 @@
 (function initializeCKEDITOR5 () 
 {
+  
+  import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+  
   var zEditorA = document.getElementById("CKEDITOR5x0");
   InlineEditor.create(zEditorA)
               .then(console.log("CKEDITOR5x0"))
               .catch( error => { console.error( error )});
     
   var zEditorB = document.getElementById("CKEDITOR5x1");
-  zEditorB.addEventListener("dblclick", 
-    function() {
-       InlineEditor.create(zEditorB)
-                   .then(console.log("CKEDITOR5x1")).catch( error => { console.error( error )})
-    }
-   );
+  
+ClassicEditor
+    .create( zEditorB )
+    .then( editor => {
+        console.log( 'Editor was initialized', editor );
+    } )
+    .catch( error => {
+        console.error( error.stack );
+    } );
 })
 ();
