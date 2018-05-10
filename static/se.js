78604c308c9ee901f8ee70d84e3041cb4e8e5471
Michael Kenneth Rasch <miketriticum@gmail.com>
2018-05-10 12:07:51 +0200

    Update se.js

--- static/se.js
+++ static/se.js
@@ -1,12 +1,16 @@
 (function initializeCKEDITOR5 () {
   
   var zEditorA = document.getElementById("CKEDITOR5x0");
+  
   InlineEditor.create(zEditorA)
               .then(console.log("CKEDITOR5x0"))
               .catch( error => { console.error( error )});
     
   var zEditorB = document.getElementById("CKEDITOR5x1");
-  InlineEditor.create( zEditorB )
+  
+  import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+
+  ClassicEditor.create( zEditorB )
               .then(console.log("CKEDITOR5x1"))
               .catch( error => { console.error( error )});
   
