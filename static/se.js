e6b6791dbd83b0f8cb7c013ea044270be3da3249
Michael Kenneth Rasch <miketriticum@gmail.com>
2018-05-29 12:37:49 +0200

    Update se.js

--- static/se.js
+++ static/se.js
@@ -13,6 +13,23 @@
               .catch( error => { console.error( error )});
   
   zEditorA.addEventListener("blur", function () {console.log(this.innerHTML)});
-
+  calendarSetup();
+})
+();
+(function calendarSetup() {
+    var zHook = document.getElementById("zCalendar"), div = document.createElement('div'); 
+    var thisDate = new Date(), temp = thisDate, firstJan = Date(thisDate);
+    firstJan.setDay() = 1, firstJan.setDate() = 1; 
+    do {    
+      if thisDate.getDay() = 1 {
+         zHook.appendChild(div) 
+         var div = document.createElement('div');      
+      } 
+      var span = document.createElement('span');
+      span.textContent = thisDate.getDate(); 
+      div.appendChild(span);
+      thisDate.setTime(parsetInt(thisDate.getTime()) + (24*60*60*1000));  
+    } while (thisDate.getFullYear() === firstJan.getFullYear());
+    zHook.appendChild(div);
 })
 ();
