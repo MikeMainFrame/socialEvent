3f18712f57747fcd316a7949aa6e7bdcc5257adb
Michael Kenneth Rasch <miketriticum@gmail.com>
2019-02-13 17:41:54 +0100

    Create GAPI.js

---
+++ js/GAPI.js
@@ -0,0 +1,58 @@
+function handleClientLoad() {
+  gapi.load('client:auth2', initClient);
+}
+function initClient() {
+  gapi.client.init({
+    apiKey: 'AIzaSyBUnSQok1aZX75FfNHRosmxN2c2WNJY7tw',
+    clientId: '545632756570-6rnj9b07bo29k8cu8002aue2t96ttci5.apps.googleusercontent.com',
+    discoveryDocs: ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest","https://www.googleapis.com/discovery/v1/apis/gmail/v1/rest"],
+    scope: 'https://www.googleapis.com/auth/calendar.readonly https://www.googleapis.com/auth/gmail.readonly',
+  }).then(function () {
+    //gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+    document.getElementById('lColumn').addEventListener("click", getEvents);
+    document.getElementById('rColumn').addEventListener("click", getMails);
+   }, function(error) {
+    appendPre(JSON.stringify(error, null, 2), 'lColumn');
+  });
+}
+function getMails() {
+ if (gapi.auth2.getAuthInstance().isSignedIn.get()) listMails(true);
+}
+function getEvents () {
+  if (gapi.auth2.getAuthInstance().isSignedIn.get()) listUpcomingEvents(true);
+}
+function appendPre(message, column) {
+  var pre = document.getElementById(column);
+  var textContent = document.createTextNode(message + '\n');
+  pre.appendChild(textContent);
+}
+function listUpcomingEvents() {
+  document.getElementById('lColumn').style.backgroundColor = "rgba(0,0,255,0.5)";
+  gapi.client.calendar.events.list({
+    'calendarId': 'primary',
+    'timeMin': (new Date()).toISOString(),
+    'showDeleted': false,
+    'singleEvents': true,
+    'maxResults': 10,
+    'orderBy': 'startTime'
+  }).then(function(response) {
+    appendPre(JSON.stringify(response, null, 2), 'lColumn');
+  });
+}
+function listMails() {
+  document.getElementById('rColumn').style.backgroundColor = "rgba(0,0,255,0.5)";
+  gapi.client.gmail.users.drafts.list({
+    'userId': 'me',
+    'includeSpamTrash': false,
+    'maxResults': 10,
+    'pageToken': "" ,
+    'q':    ""
+  }).then(function(response) {
+      gapi.client.gmail.users.drafts.get({
+      'userId': 'me',
+      'id': response.result.drafts[0].id
+      }).then(function(response) {
+        appendPre(JSON.stringify(response, null, 2), 'rColumn');
+    });
+  })
+}
