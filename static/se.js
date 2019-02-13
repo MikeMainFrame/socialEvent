311a9a467713a40bbf075528140a2c1befecbc43
Michael Kenneth Rasch <miketriticum@gmail.com>
2019-02-13 17:37:57 +0100

    Update se.js

--- static/se.js
+++ static/se.js
@@ -1,33 +1,3 @@
-(function initializeCKEDITOR5 () {
-  
-  var zEditorA = document.getElementById("CKEDITOR5x0");
-  
-  InlineEditor.create(zEditorA)
-              .then(console.log("CKEDITOR5x0"))
-              .catch( error => { console.error( error )});    
-  
-  zEditorA.addEventListener("blur", function () {console.log(this.innerHTML)});
-})
-();
-(function zcalendarSetup() {
-    return; // work in progress
-    var zHook = document.getElementById("zCalendar"), div = document.createElement('div'); 
-    var thisDate = new Date(), thisYear = thisDate.getFullYear();
-    do {    
-      if (thisDate.getDate() === 1) {
-        zHook.appendChild(div) 
-        var div = document.createElement('div');     
-        div.setAttribute("class","month");
-      } 
-      var divDate = document.createElement('div');
-      divDate.setAttribute("class","day");
-      divDate.textContent = thisDate.getDate(); 
-      div.appendChild(divDate);
-      thisDate.setTime(parseInt(thisDate.getTime()) + (24*60*60*1000));  
-    } while (thisDate.getFullYear() === thisYear);
-    zHook.appendChild(div);
-})
-();
 /***
 *  show list of dates as a band you can scroll sideways - horizontal...
 */
